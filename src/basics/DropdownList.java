f9581b34aa1fb99944fccc6fe02ab168b68313cd
dharanim04 <dharani2711@gmail.com>
2018-11-01 15:46:11 +0530

    some changes

--- src/basics/DropdownList.java
+++ src/basics/DropdownList.java
@@ -3,7 +3,6 @@ package basics;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class DropdownList {
